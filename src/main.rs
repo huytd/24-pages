c8249040a3ba81a5ed08eafd8751f156ff47ae51
Huy Tr <kingbazoka@gmail.com>
2022-04-21 01:33:56 +0000

    finally

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ pub enum OpCode {
     PUSH(i32),
     ADD,
     CMP, // 1 if equals, 0 if different
-    JMP(usize), // jump if top is 1
+    JMP_IF1(usize), // jump if top is 1
+    JMP(usize), // unconditionally jump
     PRINT,
     HALT
 }
@@ -31,7 +32,7 @@ impl VirtualMachine {
     pub fn new() -> Self {
         Self {
             program: vec![],
-            stack: vec![0; STACK_SIZE],
+            stack: Vec::with_capacity(STACK_SIZE),
             pc: 0
         }
     }
@@ -46,21 +47,67 @@ impl VirtualMachine {
             let Instruction { opcode, .. } = &self.program[self.pc];
             match opcode {
                 OpCode::PUSH(val) => {
-                    self.stack.push(*val);
+                    if self.stack.len() <= STACK_SIZE {
+                        self.stack.push(*val);
+                        self.pc += 1;
+                    } else {
+                        println!("[ERROR] Stack overflow!");
+                        is_running = false;
+                    }
                 },
                 OpCode::ADD => {
                     let (a, b) = (self.stack.pop(), self.stack.pop());
                     if a.is_some() && b.is_some() {
-
+                        self.stack.push(a.unwrap() + b.unwrap());
+                        self.pc += 1;
                     } else {
-                        println!("[ERROR] Not enough data in the stack.");
+                        println!("[ERROR] Invalid ADD call!");
+                        is_running = false;
                     }
                 },
-                OpCode::CMP => is_running = false,
-                OpCode::JMP(loc) => is_running = false,
+                OpCode::CMP => {
+                    let (a, b) = (self.stack.pop(), self.stack.pop());
+                    if a.is_some() && b.is_some() {
+                        let val = if a.unwrap() == b.unwrap() { 1 } else { 0 };
+                        self.stack.push(val);
+                        self.pc += 1;
+                    } else {
+                        println!("[ERROR] Invalid CMP call!");
+                        is_running = false;
+                    }
+                },
+                OpCode::JMP_IF1(target) => {
+                    if let Some(condition) = self.stack.pop() {
+                        if condition == 1 {
+                            if let Some(loc) = self.program.iter().position(|ins| ins.label == *target) {
+                                self.pc = loc;
+                            } else {
+                                println!("[ERROR] Jump to invalid location!");
+                                is_running = false;
+                            }
+                        } else {
+                            self.pc += 1;
+                        }
+                    } else {
+                        println!("[ERROR] Invalid JMP_IF1 call!");
+                        is_running = false;
+                    }
+                },
+                OpCode::JMP(target) => {
+                    if let Some(loc) = self.program.iter().position(|ins| ins.label == *target) {
+                        self.pc = loc;
+                    } else {
+                        println!("[ERROR] Jump to invalid location!");
+                        is_running = false;
+                    }
+                },
                 OpCode::PRINT => {
                     if let Some(data) = self.stack.pop() {
                         println!("> {}", data);
+                        self.pc += 1;
+                    } else {
+                        println!("[ERROR] Invalid PRINT call!");
+                        is_running = false;
                     }
                 },
                 OpCode::HALT => is_running = false
@@ -88,15 +135,15 @@ fn main() {
         Instruction::new(3, OpCode::ADD),               // c = a + b
         Instruction::new(4, OpCode::PUSH(30)),          
         Instruction::new(5, OpCode::CMP),               // if c == 30
-        Instruction::new(6, OpCode::JMP(10)),           
-        Instruction::new(7, OpCode::PUSH(1)),           
-        Instruction::new(8, OpCode::PRINT),             // print(1)
+        Instruction::new(6, OpCode::JMP_IF1(10)),           
+        Instruction::new(7, OpCode::PUSH(2)),           
+        Instruction::new(8, OpCode::PRINT),             // print(2)
         Instruction::new(9, OpCode::JMP(13)),
-        Instruction::new(10, OpCode::PUSH(2)),
-        Instruction::new(11, OpCode::PRINT),            // else print(2)
+        Instruction::new(10, OpCode::PUSH(1)),
+        Instruction::new(11, OpCode::PRINT),            // else print(1)
         Instruction::new(12, OpCode::JMP(13)),
         Instruction::new(13, OpCode::HALT),
     ]);
     vm.execute();
-    println!("{:#?}", vm.stack);
+    println!("STACK = {:#?}", vm.stack);
 }
